3a1cfb229ea14b87519e90f01bdcbd553376eec8
adonicristiangerman <adoni.cristiang@gmail.com>
2026-04-12 23:51:36 -0400

    finished the app

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children, open }) {
+export default function Modal({ children, open, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -14,7 +14,7 @@ export default function Modal({ children, open }) {
 
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={onClose}>
-      {children}
+      {open ? children : null}
     </dialog>,
     document.getElementById("modal"),
   );
